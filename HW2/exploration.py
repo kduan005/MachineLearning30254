c531143e81cb0c3b9f3b0a7620738760eece1483
kduan005 <36141526+kduan005@users.noreply.github.com>
2018-04-12 21:31:02 -0500

    Add files via upload

---
+++ HW2/exploration.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas.tools.plotting import scatter_matrix
+from .processing import *
+
+def read_data(path):
+    df = pd.read_csv(path)
+    return df
+
+def plot_scatter(df, vars):
+    '''
+    vars: a list of variables name, the first being the outcome
+    variable name, the rest being the independent variables of interest
+    '''
+    pd.plotting.scatter_matrix(df[vars], figsize = (12, 8))
+    plt.savefig("CRRLTN_{}_OTHRS".format(vars[0]))
+
+def corr_matrix(df, dep_var):
+    '''
+    dep_var: str, the name of the dependent variable
+    '''
+    corr_matrix = df.corr()
+    print ("Correlations between {} and other variables".format(dep_var))
+    print (corr_matrix[dep_var].sort_values(ascending = True))
+
+def plot_bar(df, var, if_continuous = False, labels = None, method = None):
+    '''
+    var: str, the variable that to be discretized
+    '''
+    if if_continuous is True:
+        discretize(df, var, bins_no, labels, method)
+        discretized_df = df[('discretized_' + var)].value_counts()
+    else:
+        discretized_df = df[var].value_counts()
+
+    discretized_df[:10].plot.bar()
+    plt.show()
